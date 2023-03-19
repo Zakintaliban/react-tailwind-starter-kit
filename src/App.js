a6289155fa69bbb6ee50bc61df81da08a3e2e5fe
Khasbullah Nukman Zakin <hasbizakin0804@gmail.com>
2023-03-19 18:03:01 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="text-center">
+      <header className="bg-slate-800 min-h-screen flex flex-col items-center justify-center text-white text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl calc(10px + 2vmin)">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="">
+          Edit <code className="font-mono">src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="text-cyan-400"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
